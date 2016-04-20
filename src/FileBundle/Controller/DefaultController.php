de57689450a46d8687eb25626c1d7c66f46e4aee
Aymericdd <aymeric42680@gmail.com>
2016-04-21 00:27:47 +0200

    Add bootstrap and FOSUserBundle with basic authentification + create layout with bootstrap + Init bundle FileBundle + override intergartion login and register

--- src/FileBundle/Controller/DefaultController.php
+++ src/FileBundle/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace FileBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        return $this->render('FileBundle:Default:index.html.twig');
     }
 }
