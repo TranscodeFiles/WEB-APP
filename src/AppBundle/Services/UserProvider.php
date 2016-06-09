e07b991be5de614e3af50b0ad5de566111a17761
Aymericdd <aymeric42680@gmail.com>
2016-06-09 22:30:38 +0200

    Fixed bug google connection

--- src/AppBundle/Services/UserProvider.php
+++ src/AppBundle/Services/UserProvider.php
@@ -3,14 +3,9 @@
 namespace AppBundle\Services;
 
 use AppBundle\Entity\User;
-use FOS\UserBundle\Event\UserEvent;
-use FOS\UserBundle\FOSUserEvents;
-use FOS\UserBundle\Model\UserManagerInterface;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\Exception\AccountNotLinkedException;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseFOSUBProvider;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface as FOSUserInterface;
 
@@ -20,17 +15,6 @@ use Symfony\Component\Security\Core\User\UserInterface as FOSUserInterface;
  */
 class UserProvider extends BaseFOSUBProvider
 {
-    /**
-     * @var EventDispatcherInterface $eventDispatcher
-     */
-    protected $eventDispatcher;
-
-    public function __construct(UserManagerInterface $userManager, array $properties, EventDispatcherInterface $dispatcher)
-    {
-        parent::__construct($userManager, $properties);
-        $this->eventDispatcher = $dispatcher;
-    }
-
 
     /**
      * {@inheritDoc}
@@ -82,22 +66,16 @@ class UserProvider extends BaseFOSUBProvider
         $user = $this->userManager->createUser();
         $this->updateUserByOAuthUserResponse($user, $response);
 
-        // set default values taken from OAuth sign-in provider account
-        if (null !== $email = $response->getEmail()) {
-            $user->setEmail($email);
-
-            $user->setEnabled(true);
-
-            $event = new UserEvent($user, new Request());
-            $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
-
-        } else {
-            $user->setEnabled(false);
-        }
-
         if (null === $this->userManager->findUserByUsername($response->getNickname())) {
             $user->setUsername($response->getNickname());
         }
+
+        // set default values taken from OAuth sign-in provider account
+        if (null !== $email = $response->getEmail()) {
+            $user->setEmail($email);
+        }
+
+        $user->setEnabled(false);
 
         return $user;
     }
